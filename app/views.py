76965808ad4ab70c9d0cadf153e405653a4f32d3
EwelinaJasielska <donnie20555@gmail.com>
2021-06-14 21:17:13 +0200

    ...

--- app/views.py
+++ app/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'index.html')
 
+def index(request):
+	return render(request, 'dodaj.html')
--- pugs/urls.py
+++ pugs/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('app.urls', namespace = 'app'))
-    path('', app.views.index)
+    path('', app.views.index),
 ]
