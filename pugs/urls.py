de61a68cf01bd62c90eb9f65b330d51b87e3072a
EwelinaJasielska <donnie20555@gmail.com>
2021-06-14 21:02:07 +0200

    simplify urls part 1

--- pugs/urls.py
+++ pugs/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls', namespace = 'app'))
+    #path('', include('app.urls', namespace = 'app'))
+    path('', app.views.index)
 ]
