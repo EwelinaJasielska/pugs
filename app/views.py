ca5e82bd3f7b4ebb51348c6155e76960ae26e0c4
EwelinaJasielska <donnie20555@gmail.com>
2021-06-14 21:57:01 +0200

    ...

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -7,8 +8,10 @@ def index(request):
 def nowy(request):
 	return render(request, 'nowy.html')
 
+@csrf_exempt
 def dodaj(request):
 	return render(request, 'index.html')
 
+@csrf_exempt
 def skasuj(request):
 	return render(request, 'index.html')
