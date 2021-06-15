93b414cb0446c0a42ebd3b845f32f1b3fe57d633
EwelinaJasielska <donnie20555@gmail.com>
2021-06-15 06:34:40 +0200

    ...

--- app/models.py
+++ app/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Pug(models.Model):
-	name = models.CharField(max_length=100)
+	imie = models.CharField(max_length=100)
 	kolor = models.CharField(max_length=100)
-	age = models.CharField(max_length=100)
+	wiek = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.imie
+	
+	def Insert(imie, kolor, wiek):
+		p = Pug(imie = imie, kolor = kolor, wiek = wiek)
+		p.save
