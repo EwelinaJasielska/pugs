fb3c1e77cda8b506cd3224974a855703ac93fed4
EwelinaJasielska <donnie20555@gmail.com>
2021-06-15 15:09:08 +0200

    ui 1

--- app/views.py
+++ app/views.py
@@ -5,31 +5,35 @@ logger = logging.getLogger('django.server')
 
 # Create your views here.
 def index(request):
-	logger.error('...index')
+	logger.info('...index')
 	return lista(request)
 
 def nowy(request):
-	logger.error('...nowy')
+	logger.info('...nowy')
 	return render(request, 'nowy.html')
 
 @csrf_exempt
 def dodaj(request):
-	logger.error('...dodaj')
+	logger.info('...dodaj')
 	imie = request.POST.get("imie", "")
-	kolor = request.POST.get("kolor", "")
-	wiek = request.POST.get("wiek", "")
-	logger.error('...dodaj imie:'+imie+ ' kolor:'+kolor+' wiek:'+wiek)
+	kolor = request.POST.get("kolor", "?")
+	wiek = request.POST.get("wiek", "?")
+	if not imie:
+		from django.contrib import messages
+		messages.info(request, 'Imie mopsa nie moze byc puste !!!')
+		return lista(request, messages)
 	from app.models import Pug
 	try:
 		p = Pug(imie = imie, kolor = kolor, wiek = wiek)
 		p.save(force_insert=True)
+		logger.info('...dodano mopsa: imie:'+imie+ ' kolor:'+kolor+' wiek:'+wiek)
 	except:
 		logger.error('...insert ERROR')
 	return lista(request)
 
 @csrf_exempt
 def skasuj(request):
-	logger.error('...skasuj')
+	logger.info('...skasuj')
 	return lista(request)
 
 def lista(request):
